5e514d84e466b01bf64bd2b89631d2c1a9b52882
rlab874 <rlabreche419@gmail.com>
2024-01-12 04:13:39 -0500

    completed NumberUtilities

--- src/main/java/com/github/curriculeon/NumberUtilities.java
+++ src/main/java/com/github/curriculeon/NumberUtilities.java
@@ -14,31 +14,70 @@ public class NumberUtilities {
         return result.toString();
     }
 
-
     public static String getOddNumbers(int start, int stop) {
-        return null;
+
+        StringBuilder result = new StringBuilder();
+
+        for(int i = start; i < stop; i++){
+            if(i % 2 == 1){
+                result.append(i);
+            }
+
+        }
+        return result.toString();
     }
 
 
     public static String getSquareNumbers(int start, int stop, int step) {
-        return null;
+        StringBuilder result = new StringBuilder();
+
+        for(int i = start; i < stop; i+=step){
+            result.append(i*i);
+        }
+
+        return result.toString();
     }
 
-    public static String getRange(int start) {
-        return null;
+    public static String getRange(int stop) {
+        StringBuilder result = new StringBuilder();
+
+        for(int i = 0; i < stop; i++){
+            result.append(i);
+        }
+
+        return result.toString();
     }
 
     public static String getRange(int start, int stop) {
-        return null;
+        StringBuilder result = new StringBuilder();
+
+        for(int i = start; i < stop; i++) {
+            result.append(i);
+        }
+
+        return result.toString();
     }
 
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+        StringBuilder result = new StringBuilder();
+
+        for(int i = start; i < stop; i+=step) {
+            result.append(i);
+        }
+
+        return result.toString();
     }
 
 
     public static String getExponentiations(int start, int stop, int step, int exponent) {
-        return null;
+
+        StringBuilder result = new StringBuilder();
+
+        for(int i = start; i < stop; i+=step) {
+            result.append((int)Math.pow(i, exponent));
+        }
+
+        return result.toString();
     }
 }
