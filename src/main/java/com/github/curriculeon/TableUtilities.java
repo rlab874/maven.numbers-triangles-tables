aadbab358a3a628d7fe12c37b2cc99324d31271c
rlab874 <rlabreche419@gmail.com>
2024-01-14 02:53:13 -0500

    finished getTriangle and getRow for TriangleUtilities

--- src/main/java/com/github/curriculeon/TableUtilities.java
+++ src/main/java/com/github/curriculeon/TableUtilities.java
@@ -2,7 +2,17 @@ package com.github.curriculeon;
 
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        return null;
+
+        StringBuilder result = new StringBuilder();
+        int size = 4;
+        int[][] table = new int[size][size];
+
+        for(int i = 1; i <= size; i++) {
+            for(int j = 1; j <= size; j++){
+                result.append(table[i-1][j-1] = i * j);
+            }
+        }
+        return result.toString();
     }
 
     public static String getLargeMultiplicationTable() {
--- src/main/java/com/github/curriculeon/TriangleUtilities.java
+++ src/main/java/com/github/curriculeon/TriangleUtilities.java
@@ -3,11 +3,27 @@ package com.github.curriculeon;
 public class TriangleUtilities {
 
     public static String getTriangle(int numberOfRows) {
-        return null;
-    }
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 1; i < numberOfRows; i++) {
+            for (int j = 1; j <= i; j++){
+            result.append('*');
+            }
+        result.append("\n");
+        }
+
+    return result.toString();
+}
+
 
     public static String getRow(int numberOfStars) {
-        return null;
+        StringBuilder result = new StringBuilder();
+
+        for(int i = 0; i < numberOfStars; i++){
+            result.append('*');
+        }
+
+        return result.toString();
     }
 
     public static String getSmallTriangle() {
